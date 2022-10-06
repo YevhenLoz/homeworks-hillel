24e0ab319d7d95a332f3193db1271bb7fc25daf0
Yevhen L <yevhen.lozovatslyi@gmail.com>
2022-10-06 09:40:32 +0300

    hw6_v2

--- hw6/hw6.py
+++ hw6/hw6.py
@@ -1,7 +1,6 @@
-#завдання 1
+#Завдання 1
 #урл http://api.open-notify.org/astros.json
 #вивести список всіх астронавтів, що перебувають в даний момент на орбіті
-# (дані не фейкові, оновлюються в режимі реального часу)
 
 import requests
 
@@ -14,12 +13,10 @@ astronauts = [astronaut['name'] for astronaut in onorbit]
 print("Astronauts on orbit now: ")
 print(*astronauts, sep="\n")
 
-#завдання 2
-
+#Завдання 2
 #апі погоди (всі токени я для вас вже прописав)
 #https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid=47503e85fabbabc93cff28c52398ae97&units=metric
 #де city_name - назва міста на аглійській мові (наприклад, odesa, kyiv, lviv)
-#результатом буде приблизно такий результат
 #погода змінюється, як і місто. яке ви введете
 #роздрукувати темпрературу та швидкість вітру. з вказівкою міста, яке було вибране
 
