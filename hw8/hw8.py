63a58b9b78aaa2a1165145ae3db7f92d15acda93
Yevhen L <yevhen.lozovatslyi@gmail.com>
2022-10-12 22:44:22 +0300

    hw8

---
+++ hw8/hw8.py
@@ -0,0 +1,31 @@
+
+def list_to_int_decorator(func):
+
+    def wrapper(*args, **kwargs):
+
+        res = func(*args, **kwargs)
+        listToStr = ' '.join([str(elem) for elem in my_list])
+        print('Result converted to string: ' + listToStr)
+        return res
+
+    return wrapper
+
+
+@list_to_int_decorator
+def my_function1(lst1):
+
+    lst1.append(1)
+
+    return lst1
+
+my_list = [1, 2, 3]
+my_list = my_function1(my_list)
+
+
+
+
+
+
+
+
+
